11028a6bd8ef4ffe096f328ed081e301bee2a2e1
Song Luo <luos@rose-hulman.edu>
2021-04-12 22:26:29 -0500

    Julia

---
+++ Julia/Basics.jl
@@ -0,0 +1,26 @@
+module Basics
+
+## String
+s = "123"
+print(s)
+
+## Tuple (immutable and unordered)
+t = (1,2,3)
+print(t[1]) #1 indexing
+
+## Dictionaries
+d = Dict("a"=>1, "b"=>2, "c"=>3)
+print(d["a"])
+
+## Array (mutable and ordered)
+a = [1, 1, 2, 3, "Ted", "Robyn"]
+
+
+## Function
+function f(x)
+    x^2
+end
+
+
+
+end
---
+++ Julia/Lenet5.jl
@@ -0,0 +1,10 @@
+module Lenet5
+using Knet, MLDatasets, IterTools
+print("sss")
+
+struct Conv; w; b; f; 
+
+end
+
+
+end
